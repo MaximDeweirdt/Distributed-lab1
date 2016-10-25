7e47a6da40a7fe3c865387c51fcccc50fffede11
Stiaan Uyttersprot <Stiaan.uyttersprot@hotmail.com>
2016-10-25 11:40:28 +0200

    Signed-off-by: Stiaan Uyttersprot <Stiaan.uyttersprot@hotmail.com>

--- lab1/src/Router/Router.java
+++ lab1/src/Router/Router.java
@@ -1,5 +1,37 @@
 package Router;
 
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Router {
 
+	public Router() { 
+		super();
+	}
+	
+	private void startRouter() {
+
+		try {
+			// create on port 1099
+			Registry registry = LocateRegistry.createRegistry(3999);
+			
+			RoutingInterface routInt = new RoutingServer();
+			
+			Naming.rebind("//[hostname]/routInt", routInt);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("dispatcher is ready on port 3999");
+	}
+	public static void main(String[] args){
+		
+		if (System.getSecurityManager() == null) {
+			 System.setSecurityManager(new SecurityManager());
+		}
+		
+		Router main = new Router();
+		main.startRouter();
+	}
+ 
 }
